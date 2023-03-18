7bb981fe75e1bffffe7d4e77fd376db5425276cb
Wiktor Czyż <czyz.wiktor@gmail.com>
2023-03-18 20:57:17 +0100

    Read from file or from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,17 @@ module Main where
 import qualified Data.Text.IO as TIO
 import Parser (pProg)
 import Text.Megaparsec (errorBundlePretty, parse)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  input <- TIO.getContents
+  args <- getArgs
+
+  -- read from file if specified
+  -- otherwise read from stdin
+  input <- case args of
+    (filename:_) -> TIO.readFile filename
+    _ -> TIO.getContents
 
   let program = parse pProg "" input
 
